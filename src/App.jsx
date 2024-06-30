bcb0d2c5e58917825e424ef5d0974cbb714d0756
dktpt44 <dhurba6don@gmail.com>
2024-06-30 18:27:07 +0400

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Construction from './components/Construction/Construction';
+
+const App = () => {
+  return (<>
+  <Navbar />
+
+  <Construction />
+
+  
+  </>);
+};
+
+export default App;
---
+++ src/components/Construction/Construction.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './construction.css';
+
+const Construction = () => {
+  return (
+    <div>
+      <div class="overlay"></div>
+      <div class="stars" aria-hidden="true"></div>
+      <div class="starts2" aria-hidden="true"></div>
+      <div class="stars3" aria-hidden="true"></div>
+      <main class="main">
+        <section class="contact">
+          <h1 class="title">NYU Embodied AI and Robotics Lab</h1>
+          <h2 class="sub-title">Site Under Construction</h2>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Construction;
