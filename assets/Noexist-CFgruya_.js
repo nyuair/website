dfb5f2f2e341d61eda3eb6e9b4cb70a5433bbf0e
dktpt44 <dhurba6don@gmail.com>
2025-05-19 13:59:42 +0545

    deploy new build

---
+++ assets/Noexist-CFgruya_.js
@@ -0,0 +1,8 @@
+import{r,j as o}from"./index-0lYHZPgo.js";const n=()=>(r.useEffect(()=>{const e=document.createElement("style");return e.innerHTML=`
+      nav {
+        padding: 4px 0;
+        border-bottom: 1px solid #dedee3;
+        backdrop-filter: blur(8px);
+        background-color: rgba(26, 27, 25, .29);
+      }
+    `,document.head.appendChild(e),()=>{document.head.removeChild(e)}}),o.jsx("div",{className:"joinusbg",children:o.jsx("div",{className:"noexitText joinusbg",children:o.jsx("h1",{children:"Coming soon!"})})}));export{n as default};
